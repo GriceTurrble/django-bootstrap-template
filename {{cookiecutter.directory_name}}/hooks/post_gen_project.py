59ec01f2a918d552568abccf4883e8331184ba62
Galen Rice <galen.s.rice@gmail.com>
2020-02-03 16:22:06 -0500

    Change templates to use raw to avoid jinja processing Also attempt a post gen script to set the secret key

---
+++ {{cookiecutter.directory_name}}/hooks/post_gen_project.py
@@ -0,0 +1,19 @@
+import random
+import os
+import re
+import fileinput
+
+def generate_key(length=50):
+    choice_set = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)"
+    return "".join([random.SystemRandom().choice(choice_set) for i in range(length)])
+
+
+target = "!!!SET_DJANGO_SECRET_KEY!!!"
+new_key = generate_key()
+filepath = os.path.join(
+    "{{cookiecutter.directory_name}}", "{{cookiecutter.directory_name}}", "settings.py"
+)
+
+with fileinput.FileInput(filepath, inplace=True, backup='.bak') as thefile:
+    for line in thefile:
+        print(line.replace(target, new_key), end='')
--- {{cookiecutter.directory_name}}/{{cookiecutter.directory_name}}/settings.py
+++ {{cookiecutter.directory_name}}/{{cookiecutter.directory_name}}/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "8zx#&twq0p(=5t$aju5mqd!xa)&q@kk)-44_qrk67)$fcrf1p2"
+SECRET_KEY = "!!!SET_DJANGO_SECRET_KEY!!!"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
