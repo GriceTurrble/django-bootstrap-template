6a4e3dd10e49695088274beab202d3fd28f467fb
Galen Rice <galen.s.rice@gmail.com>
2020-02-04 00:25:50 -0500

    Corrected missing objects

--- {{cookiecutter.project_slug}}/base_objects/models.py
+++ {{cookiecutter.project_slug}}/base_objects/models.py
@@ -2,9 +2,13 @@
 
 from django.db import models
 
+from .managers import ProjectBaseQuerySet
+
 
 class ProjectBaseModel(models.Model):
     """Base abstract model for all models used throughout the project."""
+
+    objects = ProjectBaseQuerySet.as_manager()
 
     ## Basic time tracking for all models ##
     time_created = models.DateTimeField("created", auto_now_add=True, db_index=True)
