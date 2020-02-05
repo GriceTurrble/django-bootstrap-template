d0dabf421db569648caedfb01fd6d644644d5142
Galen Rice <galen.s.rice@gmail.com>
2020-02-05 12:35:56 -0500

    Added empty `ready` method for a starting point.

--- {{cookiecutter.project_slug}}/base_objects/apps.py
+++ {{cookiecutter.project_slug}}/base_objects/apps.py
@@ -6,3 +6,10 @@ from django.apps import AppConfig
 class BaseObjectsConfig(AppConfig):
     name = "base_objects"
     verbose_name = "Project base objects"
+
+    def ready(self):
+        """Got some code you want to run once on site startup, when the app is fully loaded?
+        Here's a good spot to put it!
+        See: https://docs.djangoproject.com/en/3.0/ref/applications/#django.apps.AppConfig.ready
+        """
+        pass
