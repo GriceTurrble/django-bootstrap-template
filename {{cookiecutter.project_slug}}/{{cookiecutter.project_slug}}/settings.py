053e029fe75a9a9bf8aae26d7463c8ae0431d327
Galen Rice <galen.rice@atumbomb.com>
2020-09-05 20:53:51 -0400

    Template wasn't using the DEBUG env var. Oops!

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/settings.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG")
 ALLOWED_HOSTS = []
 
 
